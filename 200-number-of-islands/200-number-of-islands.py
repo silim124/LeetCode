92d33dbcfd8144f6c16bd9a07d8bd77af8b33b25
Sieun Lim <79463156+silim124@users.noreply.github.com>
2022-05-04 16:11:52 +0900

    Time: 599 ms (12.46%), Space: 22 MB (13.13%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -2,14 +2,13 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         ROWS, COLS = len(grid), len(grid[0])
         res = 0
-        dirs = [[1, 0], [0, 1], [-1, 0], [0, -1]]
         visited = set()
         
         def bfs(r, c):
             if r < 0 or c < 0 or r == ROWS or c == COLS or grid[r][c] == "0" or (r, c) in visited:
                 return
             visited.add((r, c))
-            for dir in dirs:
+            for dir in [[1, 0], [0, 1], [-1, 0], [0, -1]]:
                 bfs(r + dir[0], c + dir[1])
                 
         for r in range(ROWS):
